6d2adeef420f7ccda37b583d217910fd82b381c0
Natalia <natalyaivvonen@gmail.com>
2022-08-05 17:01:05 +0300

    methods created

---
+++ task1/Program.cs
@@ -0,0 +1,48 @@
+﻿string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+
+    Console.WriteLine("Creating array here: ");
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} element: ");
+        array[i] = Console.ReadLine();
+    }
+    Console.WriteLine();
+    return array;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+
+    Console.WriteLine();
+}
+
+int FindSizeOfNewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] CreateNewArray(string[] array, int size)
+{
+    string[] newArray = new string[size];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+            
+    }
+    return newArray;
+} 
